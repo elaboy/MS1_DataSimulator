eff8906dc50738df7c15f2e2843d7b829cb03311
Edwin <elaboytorres@gmail.com>
2023-03-14 15:23:50 -0500

    Update IsotopicMassesAndNormalizedAbundances.cs

--- IsotopicMassesAndNormalizedAbundances.cs
+++ IsotopicMassesAndNormalizedAbundances.cs
@@ -3,7 +3,7 @@ using Proteomics.ProteolyticDigestion;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text; 
 using System.Threading.Tasks;
 
 namespace MS1_DataSimulator
